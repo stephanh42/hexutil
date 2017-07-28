13c6a9e6cc1c8c07f3c34147902081b87f7b76c2
Stephan Houben <stephanh42@gmail.com>
2017-07-28 13:06:56 +0200

    initial commit

---
+++ hexutil/__init__.py
@@ -0,0 +1,92 @@
+"""
+Classes and functions to deal with hexagonal grids.
+
+This module assumes that the hexagonal grid is aligned with the x-axis.
+If you need it to be aligned with the y-axis instead, you will have to
+swap x and y coordinates everywhere.
+"""
+
+from collections import namedtuple
+import math
+
+class Hex(namedtuple("Hex", "x y")):
+    "A single hexagon in a hexagonal grid."""
+    _neighbours = ((2, 0), (1, 1), (-1, 1), (-2, 0), (-1, -1), (1, -1))
+
+    def is_valid(self):
+        """Check if this hex is a valid hex."""
+        x, y = self
+        return (x + y) % 2 == 0
+
+    def neighbours(self):
+        """Return the 6 direct neighbours of this hex."""
+        x, y = self
+        return [Hex(x+dx, y+dy) for dx, dy in self._neighbours]
+
+    def __add__(self, other):
+        x1, y1 = self
+        x2, y2 = other
+        return Hex(x1+x2, y1+y2)
+
+    def __sub__(self, other):
+        x1, y1 = self
+        x2, y2 = other
+        return Hex(x1-x2, y1-y2)
+
+    def __neg__(self):
+        x, y = self
+        return Hex(-x, -y)
+
+    def distance(self, other):
+        """Distance in number of hexagon steps.
+        Direct neighbours of this hex have distance 1.
+        """
+        x1, y1 = self
+        x2, y2 = other
+        dx = abs(x1 - x2)
+        dy = abs(y1 - y2)
+        return dy + max(0, (dx - dy)//2)
+
+    def rotate_left(self):
+        """Given a hex return the hex when rotated 60° counter-clock-wise around the origin.
+        """
+        x, y = self
+        return Hex((x - 3 * y) >> 1, (x + y) >> 1)
+
+    def rotate_right(self):
+        """Given a hex return the hex when rotated 60° clock-wise around the origin.
+        """
+        x, y = self
+        return Hex((x + 3 * y) >> 1, (y - x) >> 1)
+
+origin = Hex(0, 0)
+
+class HexGrid(namedtuple("HexGrid", "width height")):
+    """Represents the dimensions of a hex grid as painted on the screen.
+    The hex grid is assumed to be aligned horizontally, like so:
+       / \ / \ / \ 
+      |   |   |   |
+       \ / \ / \ /
+    The center of hex (0, 0) is assumed to be on pixel (0, 0).
+    The hexgrid is determined by width and height, which are the screen coordinates
+    of the upper-right corner of the central hex.
+
+    To have equilateral hexes, width:height should be approximately √3 : 1.
+    If you only pass in width to the constructor, the height is computed to be
+    an integer as close as possible to width / √3 .
+    """
+
+    _hex_factor = math.sqrt(1.0/3.0)
+    _corners = ((1, 1), (0, 2), (-1, 1), (-1, -1), (0, -2), (1, -1))
+
+    def __new__(cls, width, height=None):
+        if height is None:
+            height = round(cls._hex_factor * width)
+        return super().__new__(cls, width, height)
+
+    def corners(self, hex):
+        """Get the 6 corners (in pixel coordinates) of the hex."""
+        width, height = self
+        x0, y0 = hex
+        y0 *= 3
+        return [(width * (x + x0), height * (y + y0)) for x, y in self._corners]
---
+++ hexutil_test.py
@@ -0,0 +1,61 @@
+import unittest
+import hexutil
+
+class TestHex(unittest.TestCase):
+
+    def test_is_valid(self):
+        self.assertTrue(hexutil.origin.is_valid())
+        self.assertTrue(hexutil.Hex(-1, -3).is_valid())
+        self.assertFalse(hexutil.Hex(2, -3).is_valid())
+
+    def test_add(self):
+        self.assertEqual(hexutil.Hex(2, 3) + hexutil.Hex(4, 5), hexutil.Hex(6, 8))
+
+    def test_sub(self):
+        self.assertEqual(hexutil.Hex(2, 3) - hexutil.Hex(4, 6), hexutil.Hex(-2, -3))
+
+    def test_neg(self):
+        self.assertEqual(-hexutil.Hex(2, 3), hexutil.Hex(-2, -3))
+        self.assertEqual(-hexutil.origin, hexutil.origin)
+
+    def test_neighbours(self):
+        origin = hexutil.origin
+        nb = origin.neighbours()
+        self.assertEqual(len(nb), 6)
+        for h in nb:
+            self.assertTrue((-h) in nb)
+            self.assertTrue(origin in h.neighbours())
+
+    def test_distance(self):
+        h = hexutil.Hex(-1, -3)
+        for nb in h.neighbours():
+            self.assertEqual(nb.distance(nb), 0)
+            self.assertEqual(nb.distance(h), 1)
+            self.assertEqual(max(nb2.distance(h) for nb2 in nb.neighbours()), 2)
+
+    def test_rotate_left(self):
+        origin = hexutil.origin
+        neighbours = origin.neighbours()
+        nb = neighbours[0]
+        neighbours2 = []
+        for i in range(6):
+            neighbours2.append(nb)
+            nb = nb.rotate_left()
+        self.assertEqual(neighbours, neighbours2)
+
+    def test_rotate_right(self):
+        origin = hexutil.origin
+        for nb in hexutil.origin.neighbours():
+            self.assertEqual(nb.rotate_left().rotate_right(), nb)
+
+class TestHexGrid(unittest.TestCase):
+    def test_height(self):
+        self.assertEqual(hexutil.HexGrid(32).height, 18)
+
+    def test_corners(self):
+        self.assertEqual(hexutil.HexGrid(32).corners(hexutil.Hex(1, 1)), 
+                 [(64, 72), (32, 90), (0, 72), (0, 36), (32, 18), (64, 36)])
+
+
+if __name__ == '__main__':
+    unittest.main()
