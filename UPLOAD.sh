42943d1b6c430136fa64ad3bf85c9c58f271ef31
Stephan Houben <stephanh42@gmail.com>
2017-08-05 09:25:53 +0200

    plain text README

--- UPLOAD.sh
+++ UPLOAD.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git clean -x -f -d
-pandoc --from=markdown --to=rst --output=README.rst README.md
+pandoc --from=markdown --to=plain --output=README README.md && \
 python3 setup.py sdist && \
 python3 setup.py bdist_wheel && \
 python3 -m twine upload dist/*
